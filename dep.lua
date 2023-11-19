755cfebfcc7392e98481dc2f18f2359be5be33e8
MarcasRealAccount <thetremine.lumicks@gmail.com>
2023-11-19 10:22:30 +0100

    Revert previous force dependency of mimalloc
    Instead of using mimalloc directly, use the <Memory/Memory.h> interface

--- dep.lua
+++ dep.lua
@@ -3,5 +3,3 @@ local pkg = premake.extensions.pkg
 libdirs({ pkg:scriptDir() .. string.format("/Bin/%s-%s-", os.host(), pkg.arch) .. "%{cfg.buildcfg}" })
 links({ "CommonBuild" })
 externalincludedirs({ pkg:scriptDir() .. "/Inc/" })
-
-pkgdeps({ "mimalloc" })
